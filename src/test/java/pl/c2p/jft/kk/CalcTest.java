0fa25b85c86a681ede6c4ce7e9ead766eb5ded6c
Krzysztof Kozioł <Krzysztof.Koziol@fisglobal.com>
2019-07-06 02:07:05 +0200

    Initial import

---
+++ src/test/java/pl/c2p/jft/kk/CalcTest.java
@@ -0,0 +1,20 @@
+package pl.c2p.jft.kk;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class CalcTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
